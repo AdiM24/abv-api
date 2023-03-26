185d9cb00ed33a62d6a34cfc4b9825a4f7201b19
andreimota <andrei.mota2001@yahoo.com>
2023-03-26 23:59:21 +0300

    fixed middleware

--- src/middleware/register.middleware.ts
+++ src/middleware/register.middleware.ts
@@ -18,7 +18,7 @@ class RegisterMiddleware {
 
     const price = Number(req.body?.total_price);
 
-    if(register.balance < price) {
+    if(register.balance < price && req.body?.payment_type === "PLATA") {
       return res.status(400).send({ code: 400, message: "Fonduri insuficiente" });
     }
 
@@ -39,7 +39,7 @@ class RegisterMiddleware {
   async validateUserRegister(req: CustomRequest, res: express.Response, next: NextFunction) {
     const userPartners = await PartnerService.getUserPartners((req.token as any)._id);
 
-    const partner_id = req.body?.payment_type === "PLATA" ? req.body?.seller_partner_id : req.body?.buyer_partner_id;
+    const partner_id = req.body?.payment_type === "PLATA" ? req.body?.buyer_partner_id : req.body?.seller_partner_id;
 
     if(!userPartners.some(userPartner => userPartner.partner_id === partner_id)) {
       return res.status(400).send({
