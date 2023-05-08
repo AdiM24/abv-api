c86b3f64a9e5fbe09657e5dd42c05dac3826d9e3
Adrian Mota <adrian.mota@sag-digital.de>
2023-05-08 23:16:51 +0300

    Typo fix

--- src/routes/user.routes.config.ts
+++ src/routes/user.routes.config.ts
@@ -27,7 +27,7 @@ export class UserRoutes extends CommonRoutesConfig {
       .route("/api/users/series")
       .get(AuthMiddleware.auth, UsersController.getUserSeries)
       .post(AuthMiddleware.auth, UsersController.createUserSeries)
-      .delete(AuthMiddleware.auth, UserControllgier.deleteUserSeries);
+      .delete(AuthMiddleware.auth, UserController.deleteUserSeries);
 
     this.app
       .route("/api/users/series/change")
